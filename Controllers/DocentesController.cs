9861b793d9c88fc39759e6bd80d1a07d302f8846
AlbertoFernan <albealbe11@hotmail.com>
2023-05-12 14:50:02 -0600

    asd

--- Controllers/DocentesController.cs
+++ Controllers/DocentesController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SeguridadDocentesApi.Data;
 using SeguridadDocentesApi.DTO;
 
 namespace SeguridadDocentesApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class DocentesController : ControllerBase
